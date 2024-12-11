cf5aea77f9bf34c99f22b865d7a430f633d98b11
PoojaMoukhede <poojamoukhede27@gmail.com>
2024-12-11 11:39:43 +0530

    build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Footer from "./component/Footer";
 import Header from "./component/Header";
-import Main from "./component/Main";
+import Main from "./pages/Main";
 
 function App() {
   return (
